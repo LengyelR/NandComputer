ddfc746c116402ea8f0d17beba67214ad2bc64b7
lengyelr <10964681+LengyelR@users.noreply.github.com>
2019-09-18 20:25:31 +0100

    added ProgramCounter

--- cu.py
+++ cu.py
@@ -5,19 +5,55 @@ import memory
 
 class SequenceGenerator(gate.Device):
     def __init__(self):
-        self.one = [0, 1]
-        self.res = [0, 0]
         self.register = memory.Register(2)
         self.inc = ops.Increment2bit()
+        self.res = self.register.res
 
     def _wiring(self):
-        res = self.inc(self.res)
-        self.register(res, 1)
+        set_bit = 1
+        res = self.inc(self.register.res)
+        self.register(res, set_bit)
         return res
 
     def step(self):
         res = self._wiring()
         self.res = res
+
+    def __call__(self):
+        self.step()
+        return self.res
+
+
+class ProgramCounter(gate.Device):
+    def __init__(self):
+        self.input = []
+        self.inc_bit = 0
+        self.write_bit = 0
+        self.reset = 0
+        self.res = []
+
+        self.increment = ops.Increment16bit()
+        self.mux0 = gate.Multiplexer2()
+        self.mux1 = gate.Multiplexer2()
+        self.mux2 = gate.Multiplexer2()
+
+    def _wiring(self):
+        res = self.mux0(self.res, self.input, self.write_bit)
+        res = self.mux1(res, self.increment(res), self.inc_bit)
+        res = self.mux2(res, [0] * 15, self.reset)
+        return res
+
+    def step(self):
+        res = self._wiring()
+        self.res = res
+
+    def __call__(self, new_address, inc_bit, write_bit, reset):
+        self.input = new_address
+        self.inc_bit = inc_bit
+        self.write_bit = write_bit
+        self.reset = reset
+        self.step()
+        return self.res
 
 
 class InstructionDecoder(gate.Device):
@@ -50,10 +86,10 @@ class InstructionDecoder(gate.Device):
 
 class ControlUnit(gate.Device):
     def __init__(self):
-        self.ir = memory.EightBit()     # input bus: high byte of instruction register, contains opcode
+        self.ir = memory.EightBit()  # input bus: high byte of instruction register, contains opcode
         self.seq = SequenceGenerator()
         self.instruction_decoder = InstructionDecoder()
-        self.alu_flags = [0]*6          # output: input line of alu
+        self.alu_flags = [0] * 6  # output: input line of alu
 
     def step(self):
         pass
@@ -63,9 +99,7 @@ if __name__ == '__main__':
     import board
     import time
 
-    c = board.Circuit(4, SequenceGenerator)
-    c.power_on()
-    for _ in range(8):
-        print(c.device.res)
-        time.sleep(0.25)
+    c = board.Circuit(32, SequenceGenerator)
+    c.power_on(show_step=True)
+    time.sleep(5)
     c.power_off()
