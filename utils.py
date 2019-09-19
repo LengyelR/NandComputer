bacc3283d2335e52eae11411bc787498f248c609
lengyelr <10964681+LengyelR@users.noreply.github.com>
2019-09-19 20:38:21 +0100

    two's complement

--- utils.py
+++ utils.py
@@ -1,8 +1,14 @@
 def to_integer(machine_number):
-    return sum([bit*2**idx for idx, bit in enumerate(reversed(machine_number))])
+    integer = sum([bit * 2 ** idx for idx, bit in enumerate(reversed(machine_number))])
+    if machine_number[0] == 0:
+        return integer
+    else:
+        return integer - 2**16
 
 
 def to_machine_number(integer):
+    if integer < 0:
+        integer = 2**16 + integer
     return [int(bit) for bit in f'{integer:016b}']
 
 
@@ -17,4 +23,9 @@ if __name__ == '__main__':
     show(2**10)
     show(2**13+64)
     show(2**15-1)
+    show(2**15)
+
     show(2**16-1)
+    show(-1)
+    show(-3)
+    show(-18)
