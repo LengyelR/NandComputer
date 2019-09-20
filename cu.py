8fe1f85aca645bb84b6a255c34bfcbe486a0d031
lengyelr <10964681+LengyelR@users.noreply.github.com>
2019-09-20 21:15:26 +0100

    init zero bits, remove unused classes

--- cu.py
+++ cu.py
@@ -26,11 +26,11 @@ class SequenceGenerator(gate.Device):
 
 class ProgramCounter(gate.Device):
     def __init__(self):
-        self.input = []
+        self.input = [0]*16
         self.inc_bit = 0
         self.write_bit = 0
         self.reset = 0
-        self.res = []
+        self.res = [0]*16
 
         self.increment = ops.Increment16bit()
         self.mux0 = gate.Multiplexer2()
@@ -40,7 +40,7 @@ class ProgramCounter(gate.Device):
     def _wiring(self):
         res = self.mux0(self.res, self.input, self.write_bit)
         res = self.mux1(res, self.increment(res), self.inc_bit)
-        res = self.mux2(res, [0] * 15, self.reset)
+        res = self.mux2(res, [0]*16, self.reset)
         return res
 
     def step(self):
@@ -49,50 +49,11 @@ class ProgramCounter(gate.Device):
 
     def __call__(self, new_address, inc_bit, write_bit, reset):
         self.input = new_address
-        self.inc_bit = inc_bit
         self.write_bit = write_bit
+        self.inc_bit = inc_bit
         self.reset = reset
         self.step()
         return self.res
-
-
-class InstructionDecoder(gate.Device):
-    def __init__(self):
-        self.ir = memory.EightBit()
-        self.and4s = [gate.BitwiseOp(gate.And, 4) for _ in range(8)]
-        self.and3s = [gate.BitwiseOp(gate.And, 3) for _ in range(4)]
-        self.nots = [gate.Not() for _ in range(24)]
-        self.res = []
-
-    def _wiring(self):
-        res = []
-        # 0, 1, 1, 1
-        add = self.and4s[0](self.nots[0](self.ir[0]), self.ir[1], self.ir[2], self.ir[3])
-        res.append(add)
-
-        sub = self.and4s[1](self.nots[1](self.ir[0]), self.nots[2](self.ir[1]), self.ir[2], self.ir[3])
-        res.append(sub)
-        return NotImplemented
-
-    def step(self):
-        res = self._wiring()
-        self.res = res
-
-    def __call__(self, instruction):
-        self.ir = instruction
-        self.step()
-        return self.res
-
-
-class ControlUnit(gate.Device):
-    def __init__(self):
-        self.ir = memory.EightBit()  # input bus: high byte of instruction register, contains opcode
-        self.seq = SequenceGenerator()
-        self.instruction_decoder = InstructionDecoder()
-        self.alu_flags = [0] * 6  # output: input line of alu
-
-    def step(self):
-        pass
 
 
 if __name__ == '__main__':
